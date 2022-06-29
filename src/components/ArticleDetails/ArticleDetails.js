b817b261355e70cc1849d82203f1f459b6b11c5c
Bolawole <andubolawole@gmail.com>
2022-06-29 02:45:44 -0400

    Add article section

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -26,7 +26,7 @@ const ArticleDetails = () => {
         return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`;
     }
 
-    const videoDate = formatDate(articleDetails[0].timestamp);
+    const date = formatDate(articleDetails[0].timestamp);
 
     return (
         <div className='article__video-details'>
@@ -35,7 +35,7 @@ const ArticleDetails = () => {
                     By {article[0].channel}
                 </p>
                 <p className='article__video-date'>
-                    {videoDate}
+                    {date}
                 </p>
             </div>
             <div className='article__video-details--right'> 
